818b3da1ff11fb1448dc044706299b760dd61ff7
오상우 <dhtkddn3@gmail.com>
2025-01-14 17:49:57 +0900

    edit

--- pages/search_bar.py
+++ pages/search_bar.py
@@ -1,23 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
 
 #test를 입력하는 검색창을 하나 만듭니다.
 #ani_list에 있는 글자가 일부라도들어가면
 #img_list에 있는 해당 그림이 출력되는 검색창을 하나 만들어 주세요
 
-st.subheader("회사 이름과 기간을 입력하세요")
-ani_list = ['짱구는못말려', '몬스터','릭앤모티']
-img_list = ['https://i.imgur.com/t2ewhfH.png', 
-            'https://i.imgur.com/ECROFMC.png', 
-            'https://i.imgur.com/MDKQoDc.jpg']
-
-search=st.text_input("검색어를 입력하세요")
-
-
-for ani in ani_list:
-    if search in ani:
-        img_idx=ani_list.index(ani)
-if search !='':# 초기상태를 이미지없이 실행하도록
-    st.image(img_list(img_idx))
-st.date_input("시작일-종료일")
-st.button("주가 데이터 확인")
-
+# 사이드바 입력
+st.sidebar.markdown("### 회사 이름과 기간을 입력하세요")
+company = st.sidebar.text_input("회사 이름", value="삼성전자")
+date_range = st.sidebar.date_input(
+    "시작일 - 종료일",
+    value=(datetime.date(2019, 1, 1), datetime.date(2021, 12, 31)),
+)
+fetch_button = st.sidebar.button("주가 데이터 확인")
