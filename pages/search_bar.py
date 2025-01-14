8f5c6dc206aac20aae9f83192c568caf20fb36cb
오상우 <dhtkddn3@gmail.com>
2025-01-14 17:22:34 +0900

    edit

--- pages/search_bar.py
+++ pages/search_bar.py
@@ -3,6 +3,8 @@ import streamlit as st
 #test를 입력하는 검색창을 하나 만듭니다.
 #ani_list에 있는 글자가 일부라도들어가면
 #img_list에 있는 해당 그림이 출력되는 검색창을 하나 만들어 주세요
+
+st.text("회사 이름과 기간을 입력하세요")
 ani_list = ['짱구는못말려', '몬스터','릭앤모티']
 img_list = ['https://i.imgur.com/t2ewhfH.png', 
             'https://i.imgur.com/ECROFMC.png', 
